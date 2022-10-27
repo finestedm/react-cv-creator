ce15babca40e2bf2d703103365b7928f80c19b09
Paweł Stępień <finestedm@gmail.com>
2022-10-27 22:36:31 +0200

    small fixes

--- src/App.js
+++ src/App.js
@@ -88,7 +88,14 @@ export default class App extends React.Component {
     return (
       <>
       <nav className="navbar navbar-light bg-light sticky-top">
-        <span className="navbar-brand mb-0 p-3 h1">CV Creator</span>
+          <span className="navbar-brand mb-0 p-3 h1">CV Creator</span>
+          <a href='https://github.com/finestedm'>
+            <span className="navbar-brand mb-0 p-3">Created by Paweł Stępień
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-github" viewBox="0 0 20 20">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+              </svg>
+            </span>
+          </a>
       </nav>
       <Container fluid="md" className='mt-5'>
         <Row className='main d-flex justify-content-around'>
@@ -101,12 +108,12 @@ export default class App extends React.Component {
             <Row className='mb-4'>
               <h4 className='p-0'>Education</h4>
               {Object.values(inputs.education).map(input => <EducationInput {...input} handleInput={this.handleInput} deleteEntry={this.deleteEntry} />)}
-              <Button variant='outline-secondary' onClick={(e) => this.addNewEntry('education')}>+ Add another education entry</Button>
+              <Button variant='outline-secondary' onClick={() => this.addNewEntry('education')}>+ Add another education entry</Button>
             </Row>
             <Row className='mb-4'>
               <h4 className='p-0'>Work experience</h4>
               {Object.values(inputs.workExperience).map(input => <WorkInput {...input} handleInput={this.handleInput} deleteEntry={this.deleteEntry} />)}
-              <Button variant='outline-secondary' onClick={(e) => this.addNewEntry('workExperience')}>+ Add another job</Button>
+              <Button variant='outline-secondary' onClick={() => this.addNewEntry('workExperience')}>+ Add another job</Button>
             </Row>
           </Col>
           <Col>
